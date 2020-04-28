eaafd628c57168780d8a07957f31815ae7570d2f
Hansel <hanselcapillo@hotmail.com>
2020-04-28 17:15:14 -0500

    se agrega la alerta

--- src/app/pagina2/pagina2.module.ts
+++ src/app/pagina2/pagina2.module.ts
@@ -7,14 +7,11 @@ import { IonicModule } from '@ionic/angular';
 import { Pagina2PageRoutingModule } from './pagina2-routing.module';
 
 import { Pagina2Page } from './pagina2.page';
+import { AlertController } from '@ionic/angular';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    Pagina2PageRoutingModule
-  ],
-  declarations: [Pagina2Page]
+  imports: [CommonModule, FormsModule, IonicModule, Pagina2PageRoutingModule],
+  declarations: [Pagina2Page],
+  providers: [AlertController],
 })
 export class Pagina2PageModule {}
--- src/app/pagina2/pagina2.page.ts
+++ src/app/pagina2/pagina2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina2',
@@ -6,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina2.page.scss'],
 })
 export class Pagina2Page implements OnInit {
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
+
+  public async openAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Desea pasar a la pagina 3?',
+      buttons: [
+        {
+          text: 'NO',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'SI',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.router.navigate(['pagina3']);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
